e68a9c6986a20206b1ce782e63316bd0d731089f
yuzgit <ofnmk.xcode.05@gmail.com>
2020-11-02 23:12:39 +0900

    if not exist setting file, create there

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
+import os
 import sys
+import argparse
 
 
 def main():
-    print(sys.argv[1])
+    p = argparse.ArgumentParser()
+    p.add_argument('cmd', help='ls, rm, add')
+    # p.add_argument('-a', '--opt_a', help='option')
+    args = p.parse_args()
+
+    envPath = os.environ['HOME'] + '/.cstash'
+    envExist = os.path.exists(envPath)
+
+    if not envExist:
+        f = open(envPath, 'w')
+        f.write('')
+        f.close()
+
+    if args.cmd == 'add':
+        print('add exec')
+    if args.cmd == 'ls':
+        print('ls exec')
+    if args.cmd == 'rm':
+        print('rm exec')
